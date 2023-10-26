93a6616647da9758b3392eb021943ce5e4675e9b
Mihail Stamenov <michaelstamenov911@gmail.com>
2023-10-26 19:09:10 +0300

    Change order of animals (testing git)

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Hello, React!"),
     React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
       breed: "Cockatiel",
     }),
-    React.createElement(Pet, {
+    React.createElement(Pet,{
       name: "Doink",
       animal: "Cat",
       breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
     }),
   ]);
 };
