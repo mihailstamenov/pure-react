c47228e904448ac99cb94e383d9c41666c9af0d5
Mihail Stamenov <michaelstamenov911@gmail.com>
2023-11-03 00:22:42 +0200

    Add useBreedList custom hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
+  const [pets, setPets] = useState([]);
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -15,9 +16,10 @@ const SearchParams = () => {
 
   async function requestPets() {
     const res = await fetch(
-      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
+
     setPets(json.pets);
   }
 
@@ -66,7 +68,7 @@ const SearchParams = () => {
           Breed
           <select
             id="breed"
-            disabled={breeds.length === 0}
+            disabled={!breeds.length}
             value={breed}
             onChange={(e) => setBreed(e.target.value)}
             onBlur={(e) => setBreed(e.target.value)}
@@ -74,11 +76,12 @@ const SearchParams = () => {
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
-                {breed}{" "}
+                {breed}
               </option>
             ))}
           </select>
         </label>
+
         <button>Submit</button>
       </form>
       {pets.map((pet) => (
