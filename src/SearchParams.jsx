b50cdb256a5567047a8104543a956b9e62e558f4
Mihail Stamenov <michaelstamenov911@gmail.com>
2023-11-02 22:56:34 +0200

    Add dependency array in useEffect

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -11,7 +11,7 @@ const SearchParams = () => {
 
   useEffect(() => {
     requestPets();
-  });
+  }, []);
 
   async function requestPets() {
     const res = await fetch(
@@ -23,7 +23,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
